b53c12a81177dd73c47c1ecfb6ba2ae8b70be307
Lilian CHIROL <lilian.chirol2021@campus-eni.fr>
2021-10-28 10:57:57 +0200

    quelques changements

--- src/main/java/fr/lihalie/TpPotager/bll/potager/PotagerManager.java
+++ src/main/java/fr/lihalie/TpPotager/bll/potager/PotagerManager.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import fr.lihalie.TpPotager.bo.Potager;
 
-public interface PotagerManager {
+public interface PotagerManager   {
 	public void addPotager(Potager potager);
 
 	public List<Potager> getAllPotager();
